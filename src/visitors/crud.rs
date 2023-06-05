2dd5de954d675f12cb36bbc146fcaadffca79e6a
AlesiRowland <alesi.rowland@essentia-analytics.com>
2023-06-05 15:27:02 +0100

    tests: for file_remover

--- src/visitors/crud.rs
+++ src/visitors/crud.rs
@@ -3,6 +3,27 @@ use std::error::Error;
 use std::path::Path;
 use crate::file_system::files::FileVisitor;
 
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io;
+    use tempdir::TempDir;
+    use crate::file_system::files::FileVisitor;
+    use crate::visitors::crud::FileRemover;
+
+    #[test]
+    fn test_file_remover_visit_file() -> Result<(), io::Error>{
+        let tempdir = TempDir::new("test")?;
+        let path = tempdir.path().join("tmp");
+        File::create(&path)?;
+        FileRemover.visit_file(&path).unwrap();
+        let left = path.exists();
+        let right = false;
+        assert_eq!(left, right);
+        Ok(())
+
+    }
+}
 pub struct FileRemover;
 
 impl FileVisitor for FileRemover {
